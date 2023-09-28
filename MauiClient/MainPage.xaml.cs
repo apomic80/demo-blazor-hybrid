4cec3e196101b775421445bb00d679f5b5813e02
apomic80 <michele.aponte@blexin.com>
2023-09-28 23:45:44 +0200

    demo 04

--- MauiClient/MainPage.xaml.cs
+++ MauiClient/MainPage.xaml.cs
@@ -1,9 +1,32 @@
-﻿namespace MauiClient;
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MauiClient;
 
 public partial class MainPage : ContentPage
 {
+    private readonly List<string> _items = new();
+
+    public void RemoveItemFromList(string item)
+    {
+        _items.Remove(item);
+    }
+
+    private void AddItemToList(string item)
+    {
+        _items.Add(item);
+    }
+    
     public MainPage()
     {
         InitializeComponent();
+        
+        rootComponent.Parameters = 
+            new Dictionary<string, object>
+            {
+                { "Items", _items },
+                { "OnAddItemRequested", new EventCallbackFactory().Create<string>(this, AddItemToList) }, 
+                { "OnDeleteItemRequested", new EventCallbackFactory().Create<string>(this, RemoveItemFromList)  }
+            };
+        
     }
 }
